9c4bd03ace23714727b6ea4f8a4562896042b24a
MarkEASV <achson01@easv365.dk>
2025-09-09 14:37:50 +0200

    pages

---
+++ wp-content/themes/kanten/page-blogsview.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main class="contentBlogs">
+    <h1>ALle blogs</h1>
+    <p>This is a sample page about sustainability initiatives. You can add your own content here.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae velit ex.</p>
+</main>
+<?php get_footer(); ?>
