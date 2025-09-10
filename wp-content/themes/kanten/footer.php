4aed6dd1a6ca94ec8bb56bf89fb890c1950b4c9f
SiegVive <vicmor01@easv365.dk>
2025-09-10 16:11:56 +0200

    footer touch up

--- wp-content/themes/kanten/footer.php
+++ wp-content/themes/kanten/footer.php
@@ -3,14 +3,14 @@
         <div class="footerGrid">
             <div class="footerCustomerService">
                 <ul class="footerUL">
-                    <li class="footerTitle"><p><?php pll_e("Kundeservice")?></p><div class="footerLine"></div></li>
+                    <li class="footerTitle"><h3><?php pll_e("Kundeservice")?></h3><div class="footerLine"></div></li>
                     <li class="footerContact"><img class="footerPic" src="<?php echo get_template_directory_uri(); ?>/assets/img/kantenLogo_white_transparent.webp" alt="Kanten"><p>+45 6767-6767</p></li>
                     <li class="footerContact"><img class="footerPic" src="<?php echo get_template_directory_uri(); ?>/assets/img/kantenLogo_white_transparent.webp" alt="Kanten"><p>Kanten@Kanten.dk</p></li>
                 </ul>
             </div>
             <div class="footerAbout">
                 <ul class="footerUL">
-                    <li class="footerTitle"><p>Kanten</p><div class="footerLine"></div></li>
+                    <li class="footerTitle"><h3>Kanten</h3><div class="footerLine"></div></li>
                     <li><a href="#"><?php pll_e("Om kanten")?></a></li>
                     <li><a href="#"><?php pll_e("Bliv medlem")?></a></li>
                     <li><a href="#"><?php pll_e("Bestyrelsen")?></a></li>
@@ -18,7 +18,7 @@
             </div>
             <div class="footerService">
                 <ul class="footerUL">
-                    <li class="footerTitle"><p>Service</p><div class="footerLine"></div></li>
+                    <li class="footerTitle"><h3>Service</h3><div class="footerLine"></div></li>
                     <li><a href="#"><?php pll_e("Kontakt")?></a></li>
                     <li><a href="#"><?php pll_e("Privatlivspolitik")?></a></li>
                     <li><a href="#"><?php pll_e("Login")?></a></li>
