9c0b6305a1cc61387aed44413922c727a316fa4c
SiegVive <vicmor01@easv365.dk>
2025-09-10 16:00:25 +0200

    Made the footer, (HTML, CSS)

--- wp-content/themes/kanten/footer.php
+++ wp-content/themes/kanten/footer.php
@@ -1,7 +1,29 @@
 <?php wp_footer(); ?>
     <div class="footerContainer">
         <div class="footerGrid">
-            
+            <div class="footerCustomerService">
+                <ul class="footerUL">
+                    <li class="footerTitle"><p>Kundeservice</p><div class="footerLine"></div></li>
+                    <li class="footerContact"><img class="footerPic" src="<?php echo get_template_directory_uri(); ?>/assets/img/kantenLogo_white_transparent.webp" alt="Kanten"><p>+45 6767-6767</p></li>
+                    <li class="footerContact"><img class="footerPic" src="<?php echo get_template_directory_uri(); ?>/assets/img/kantenLogo_white_transparent.webp" alt="Kanten"><p>Kanten@Kanten.dk</p></li>
+                </ul>
+            </div>
+            <div class="footerAbout">
+                <ul class="footerUL">
+                    <li class="footerTitle"><p>Kanten</p><div class="footerLine"></div></li>
+                    <li><a href="#">Om kanten</a></li>
+                    <li><a href="#">Bliv medlem</a></li>
+                    <li><a href="#">Bestyrelsen</a></li>
+                </ul>
+            </div>
+            <div class="footerService">
+                <ul class="footerUL">
+                    <li class="footerTitle"><p>Service</p><div class="footerLine"></div></li>
+                    <li><a href="#">Kontakt</a></li>
+                    <li><a href="#">Privatlivspolitik</a></li>
+                    <li><a href="#">Login</a></li>
+                 </ul>
+            </div>
     </div>
 </body>
 </html>
