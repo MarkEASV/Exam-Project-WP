3368cf652c2c84820bc7612366d1caaa2b64a8fe
faxe-kondi <noakri01@easv365.dk>
2025-09-10 14:12:11 +0200

    fixed evil functions.php push + mysterious head html

--- wp-content/themes/kanten/page-blogsview.php
+++ wp-content/themes/kanten/page-blogsview.php
@@ -1,12 +1,4 @@
 <?php get_header(); ?>
-<<<<<<< HEAD
-
-<main class="contentBlogs">
-    <h1>ALle blogs</h1>
-    <p>This is a sample page about sustainability initiatives. You can add your own content here.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae velit ex.</p>
-</main>
-=======
 <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post(); ?>
         
