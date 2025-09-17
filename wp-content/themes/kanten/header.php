808e3760d2a4c4a796fe66dae0a35822405a47e9
MarkEASV <achson01@easv365.dk>
2025-09-17 09:18:29 +0200

    Deleted doubled of wp_head

--- wp-content/themes/kanten/header.php
+++ wp-content/themes/kanten/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('Kanten'); ?></title>
-    <?php wp_head(); ?>
+    <?php ?>
 </head>
     <nav class="navBar">
     <!-- <img src="" alt=""> -->
