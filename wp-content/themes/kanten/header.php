9c60387f724723c17322a57bbd7f8b373c6e4d7d
SiegVive <vicmor01@easv365.dk>
2025-09-24 13:55:55 +0200

    perf(partials): removed code fill

--- wp-content/themes/kanten/header.php
+++ wp-content/themes/kanten/header.php
@@ -7,20 +7,20 @@
     <title><?php bloginfo('Kanten'); ?></title>
     <?php ?>
 </head>
-    <nav class="navBar" aria_labbelledby="navLanguages navEvents navBlogs navSustain navAbout">
+    <nav class="navBar">
         <div class="navBarGrid">
             <div class="logoImg">
                 <a href="<?php echo home_url(); ?>">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/kantenLogo_white_transparent.webp" alt="Kanten Logo">
                 </a>
             </div>
-                <ul class="languages"><li id="navLanguages"><?php pll_the_languages() ?></li></ul>
+                <ul class="languages"><li><?php pll_the_languages() ?></li></ul>
             <ul class="ulNav">
-                <li><a id="navEvents" href="#"><?php pll_e("events") ?></a></li>
-                <li><a id="navBlogs" href="<?php echo site_url('/blogsview/'); ?>"><?php pll_e("blogs") ?></a></li>
-                <li><a id="navSustain" href="<?php echo site_url('/sustainability-initiatives/'); ?>"><?php pll_e("bæredygtighed") ?></a></li>
-                <li><a id="navMerch" href="<?php echo site_url('/merchview/'); ?>"><?php pll_e("Merch") ?></a></li>
-                <li><a id="navAbout" href="#"><?php pll_e("om") ?></a></li>
+                <li><a href="#"><?php pll_e("events") ?></a></li>
+                <li><a href="<?php echo site_url('/blogsview/'); ?>"><?php pll_e("blogs") ?></a></li>
+                <li><a href="<?php echo site_url('/sustainability-initiatives/'); ?>"><?php pll_e("bæredygtighed") ?></a></li>
+                <li><a href="<?php echo site_url('/merchview/'); ?>"><?php pll_e("Merch") ?></a></li>
+                <li><a href="#"><?php pll_e("om") ?></a></li>
             </ul>
         </div>
     </nav>   
