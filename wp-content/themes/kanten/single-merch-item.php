9d9c51a3899a0338ee4318d857fd42dd719f3da2
faxe kondi <noakri01@easv365.dk>
2025-09-24 14:01:21 +0200

    docs(style) gave footer a bigger margin-top. made more products section in single-merch-item

--- wp-content/themes/kanten/single-merch-item.php
+++ wp-content/themes/kanten/single-merch-item.php
@@ -40,7 +40,7 @@
                                     <h3><?php echo esc_html($categoryLabel); ?></h3>
                                     <p>kr <?php echo esc_html($merchPrice); ?>,-</p>
                                     <div class="sizesArea">
-                                            
+
                                     </div>
                                     <div class="merchItemTextAreaBottom">
                                         <form class="buyForm" method="post" action="">
@@ -59,8 +59,53 @@
                                     <?php echo $merchDescription; ?>
                                 </div>
                             </section>
+                            <section class="otherItemsSection">
+                              <h3>Andre produkter</h3>
+                                <?php
+                                    $args = array(
+                                    'post_type'      => 'merch-item',
+                                    'posts_per_page' => 3,
+                                    'lang' => pll_current_language(),
+                                    );
+
+                                    $loop = new WP_Query($args);
+
+                                    if ($loop->have_posts()) :
+                                    while ($loop->have_posts()) : $loop->the_post();
+                                        
+                                        $merchImage = get_field('merch_image_main');
+                                        $merchTitle = get_the_title();
+                                        $merchPrice = get_field('merch_item_price');
+                                        $merchCategory = get_field('merch_item_category');
+
+                                                if ($merchCategory) {
+                                                    if (is_object($merchCategory)) {
+                                                    $categoryLabel = $merchCategory->name;
+                                                                            }
+                                                }
+                                ?>
+                            <a href="<?php the_permalink(); ?>">
+                                
+                                <div class="merchCard">
+                                <div class="merchCardImageContainer">
+                                    <img src="<?php echo esc_url($merchImage['url'])?>" alt="<?php echo esc_attr($merchImage['alt']); ?>">
+                                </div>
+                                <h5><?php echo esc_html($categoryLabel); ?></h5>
+                                <h4><?php echo esc_html($merchTitle); ?></h4>
+                                <div class="merchPriceContainer"><p>kr <?php echo esc_html($merchPrice); ?>,-</p></div>
+
+                        
+                                </div>
+                            </a>
+                                <?php
+                                    endwhile;
+                                    endif;
+                                    wp_reset_postdata();
+                                ?>
+                            </section>
                         </section>
 
+                                            
 
 
 
