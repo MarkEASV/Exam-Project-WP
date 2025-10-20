0c7aaa6446d57c67911794b72e8592c8cc428fbd
Catalin4Florin <catroa01@easv365.dk>
2025-10-20 10:00:39 +0200

    plugin update

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1759939091;s:7:"authKey";s:64:"KXe;qoT*8*Ggog59+CuGKwt;5E<[~WDLcj_,yOx>JZ@K# E^cwH0j.vff>z,YeJC";s:7:"version";s:5:"1.1.0";}
+a:4:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"KXe;qoT*8*Ggog59+CuGKwt;5E<[~WDLcj_,yOx>JZ@K# E^cwH0j.vff>z,YeJC";s:7:"version";s:5:"1.1.0";}
