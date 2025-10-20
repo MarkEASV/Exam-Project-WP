d62f6b84e1f10d703770a6a483f16111ddac373d
faxe-kondi <noakri01@easv365.dk>
2025-10-20 22:08:12 +0200

    perf(layout, style): change headings on blog cards to improve hierarchy + implement css to make blog cards identical on all pages

--- wp-content/themes/kanten/page-blogsview.php
+++ wp-content/themes/kanten/page-blogsview.php
@@ -3,53 +3,42 @@
         <?php while(have_posts()): the_post(); ?>
         
       <section class="blogviewSection">
-        <h2 tabindex="0"><?php pll_e("alle blogindlæg") ?></h2>
+        <h1 tabindex="0"><?php pll_e("alle blogindlæg") ?></h1>
           <?php
-            $args = array(
-              'post_type'      => 'blog',
-              'posts_per_page' => -1,
-              'lang' => pll_current_language(),
-            );
-
-            $loop = new WP_Query($args);
-
-            if ($loop->have_posts()) :
-              while ($loop->have_posts()) : $loop->the_post();
-                
-                $blogImage = get_field('blog_image');
-                
-                $blogTitle = get_the_title();
-                $blogText = get_field('blog_card_text');
-                $blogAuthor = get_the_author_meta('display_name');
-                $blogDate  = get_the_date('d-m-Y');
-                $blogCategory = get_field('blog_category');
-
-                        if ($blogCategory) {
-                            if (is_object($blogCategory)) {
-                            $categoryLabel = $blogCategory->name;
-                                                    }
-                        }
-          ?>
-      <a href="<?php the_permalink(); ?>" aria-labbelledby="blogTitle blogText">
-        
-        <div class="blogCard">
-          <div class="CardImageContainer">
-            <img src="<?php echo esc_url($blogImage['url'])?>" alt="<?php echo esc_attr($blogImage['alt']); ?>">
-          </div>
-          <h3 id="blogTitle"><?php echo esc_html($blogTitle); ?></h3>
-          <h4><?php echo esc_html($categoryLabel); ?></h4>
-          <div class="blogCardDetails">
-            <small class="blogAuthor"><?php pll_e("af") ?> <?php echo esc_html($blogAuthor); ?></small>
-            <small class="blogDate"><?php echo esc_html($blogDate); ?></small>
-          </div>
-          <div id="blogText" class="CardText"><?php echo wp_kses_post($blogText); ?></div>
-        </div>
-      </a>
-          <?php
-              endwhile;
-            endif;
-            wp_reset_postdata();
-          ?>
+          $args = [
+            'post_type' => 'blog',
+            'posts_per_page' => -1,
+            'lang' => pll_current_language(),
+          ];
+          $loop = new WP_Query($args);
+          if ($loop->have_posts()) :
+            while ($loop->have_posts()) : $loop->the_post();
+              $blogImage   = get_field('blog_image');
+              $blogTitle   = get_the_title();
+              $blogText    = get_field('blog_card_text');
+              $blogAuthor  = get_the_author_meta('display_name');
+              $blogDate    = get_the_date('d-m-Y');
+              $blogCategory = get_field('blog_category');
+              $categoryLabel = is_object($blogCategory) ? $blogCategory->name : '';
+              $blogID = 'blogTitle-' . get_the_ID();
+        ?>
+        <a href="<?php the_permalink(); ?>" aria-labelledby="blogHeading">
+          <article class="blogCard">
+            <div class="CardImageContainer">
+              <?php echo wp_get_attachment_image($blogImage['ID'], 'blog-thumb'); ?>
+            </div>
+            <h2 id="<?php echo $blogID; ?>"><?php echo esc_html($blogTitle); ?></h2>
+            <?php if ($categoryLabel): ?>
+              <p class="blogCategory"><?php echo esc_html($categoryLabel); ?></p>
+            <?php endif; ?>
+            <div class="blogCardDetails">
+              <small class="blogAuthor"><?php pll_e("af_front"); ?> <?php echo esc_html($blogAuthor); ?></small>
+              <small class="blogDate"><?php echo esc_html($blogDate); ?></small>
+            </div>
+            <p class="CardText"><?php echo wp_kses_post($blogText); ?></p>
+          </article>
+        </a>
+        <?php endwhile; endif; wp_reset_postdata(); ?>
               
 
 
